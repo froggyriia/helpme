e0433a3e5bc93f3bcbf01999c8b83138adec67a9
froggyriia <nira.osamu@yandex.ru>
2023-06-08 10:47:05 +0300

    add .gitignore file

--- meme.py
+++ meme.py
@@ -1,10 +1,12 @@
 # объукт Человек и Мемы. кто то листает одни мемы другой другие. из разных источников.
 from PIL import Image
 from dataclasses import dataclass, field
+import requests
 
 
 @dataclass
 class Meme:
+    link = 'https://api.thecatapi.com/v1/images/search'
     image: Image = None
     likes_amount: int = 0
     text: str = ""  # подпись
@@ -12,6 +14,17 @@ class Meme:
 
     def show_meme(self, consumer: 'Person'):
         consumer.consume(self)
+
+        answer: list[dict: str | int] = requests.get(Meme.link).json()
+        url = answer[0].get('url')
+        # print(answer, url, sep= '\n') я чисто чекнула то у менявообще в переменных или хуйня какая-то лежит
+        frmt = url.rsplit(".")[-1]
+        data = requests.get(url).content
+        f = open(f'img.{frmt}', 'wb')
+        f.write(data)
+        f.close()
+        img = Image.open(f'img.{frmt}')
+        img.show()
 
     def like_meme(self):
         self.likes_amount += 1
@@ -43,19 +56,20 @@ class Person:
         target.react_on_stupid_meme(meme)
 
 
+#
 friend1 = Person('biba')
-friend2 = Person('boba')
-friend3 = Person('Hitler')
-friend3.add_friend(friend1)
-friend1.add_friend(friend3)
-friend1.add_friend(friend2)
-print(friend1.friends)
-uncat_meme = Meme(text='unfunny text')
-
-
-cat_meme = Meme(text='funny text', is_funny= True)
-friend1.consume(uncat_meme)
-friend1.share_meme(friend2, uncat_meme)
-friend1.consume(cat_meme)
-cat_meme.like_meme()
-print(cat_meme.likes_amount)
+# friend2 = Person('boba')
+# friend3 = Person('Hitler')
+# friend3.add_friend(friend1)
+# friend1.add_friend(friend3)
+# friend1.add_friend(friend2)
+# print(friend1.friends)
+# uncat_meme = Meme(text='unfunny text')
+#
+cat_meme = Meme(text='funny text', is_funny=True)
+# friend1.consume(uncat_meme)
+# friend1.share_meme(friend2, uncat_meme)
+# friend1.consume(cat_meme)
+# cat_meme.like_meme()
+# print(cat_meme.likes_amount)
+cat_meme.show_meme(friend1)
---
+++ tg_bot_finally_or_not.py
@@ -0,0 +1,28 @@
+from aiogram import Bot, Dispatcher, types, executor
+import logging
+from env import TOKEN
+from aiogram.types import Message
+
+token = TOKEN
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token)
+dp = Dispatcher(bot)  # осдержит все обработчике и ответчики
+
+
+@dp.message_handler(commands=['start', 'help'])  # содержит команды на которые он будет отвечать
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.reply("Hi! \nI'm a meow_catty_bot!")
+
+
+@dp.message_handler()
+async def echo(message: Message):
+    await message.answer(message.text + f' meow, {message.from_user["first_name"]}')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
