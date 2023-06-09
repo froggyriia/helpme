807b22f5ee056009fee63d3572ac546b63c00fcf
froggyriia <nira.osamu@yandex.ru>
2023-06-09 09:19:02 +0300

    bot without FSM

--- tg_bot_finally_or_not.py
+++ tg_bot_finally_or_not.py
@@ -1,12 +1,20 @@
+import matplotlib
 from aiogram import Bot, Dispatcher, types, executor
 import logging
 from env import TOKEN
 from aiogram.types import Message
+from numexpr import evaluate
+import numpy as np
+import matplotlib.pyplot as plt
+matplotlib.use("Agg")
+import io
+from PIL import Image
+import io
+plt.ioff()
+pi = np.pi
+e = np.e
 
-
-token = TOKEN #вставить сюда свой токен
-
-
+token = TOKEN  # вставить сюда свой токен
 
 logging.basicConfig(level=logging.INFO)
 
@@ -14,8 +22,27 @@ bot = Bot(token)
 dp = Dispatcher(bot)  # осдержит все обработчике и ответчики
 
 
+def fig2buf(fig):
+    buf = io.BytesIO()
+    fig.savefig(buf)
+    buf.seek(0)
+    return buf
+
+
+def plot_formula(formula: str, bounds: tuple[float, float], estimations: int):
+    x = np.linspace(*bounds, estimations)
+    y = evaluate(formula)
+    fig, ax = plt.subplots()
+    ax.grid()
+    ax.axis()
+    ax.plot(x, y, label=formula)
+    ax.legend()
+    img = fig2buf(fig)
+    return img
+
+
 @dp.message_handler(commands=['start', 'help'])  # содержит команды на которые он будет отвечать
-async def echo(message: types.Message):
+async def _(message: types.Message):
     # old style:
     # await bot.send_message(message.chat.id, message.text)
 
@@ -23,10 +50,11 @@ async def echo(message: types.Message):
 
 
 @dp.message_handler()
-async def echo(message: Message):
-    await message.answer(message.text + f' meow, {message.from_user["first_name"]}')
+async def _(message: Message):
+    img = plot_formula(message.text, (-5, 5), 100) # FSM
+    await message.answer_photo(img)
+    img.close()
 
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-
